bd2ffbb2fbf55015515b045284811ad52fd4f959
ridewithharsh46-art <ridewithharsh46@gmail.com>
2026-02-26 22:34:43 +0530

    Create Password-Retry-System.py

---
+++ module2/Password-Retry-System.py
@@ -0,0 +1,9 @@
+# Keep asking for password until correct one is entered
+while True:
+    password = input("Enter password: ")
+    
+    if password == "admin123":
+        print("Access granted")
+        break
+    else:
+        print("Incorrect password, try again.")
