3716291cfe67154d32c25d613585de60a30ce9a9
ridewithharsh46-art <ridewithharsh46@gmail.com>
2026-02-27 22:49:20 +0530

    Create Add-two-number.py

---
+++ module2/Add-two-number.py
@@ -0,0 +1,13 @@
+# Define the function with parameters
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# Main code
+number1 = 7
+number2 = 8
+
+# Call the function
+result = add_numbers(number1, number2)
+
+# Print the result
+print("Sum is:", result)
